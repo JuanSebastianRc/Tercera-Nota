18a125aedd98a6b9433bc6a49ded64ddb0b7c6b0
JuanSebastianRc <jsroperoc@ufpso.edu.co>
2025-09-03 19:51:57 -0500

    salary

---
+++ salaries.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class salaries {
+    public String name;       
+    private double salary;    
+    protected String role;    
+
+    int level;  
+
+    public public void setSalary(double salary) {
+        this.salary = salary;
+        if (salary>0) {
+            System.out.println();
+        }else{
+            System.out.println();
+    }
+
+     public double getSalary() {
+        return salary;
+    }
+  }
+}
